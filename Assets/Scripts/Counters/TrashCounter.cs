a803c3beb4fc6edd67f1e65223f09f04d82c1979
hmuaz <muazsenol1@gmail.com>
2023-04-07 02:50:59 +0300

    Sync Cutting Counter

--- Assets/Scripts/Counters/TrashCounter.cs
+++ Assets/Scripts/Counters/TrashCounter.cs
@@ -1,25 +1,41 @@
 using System;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.Netcode;
 using UnityEngine;
 
-public class TrashCounter : BaseCounter {
+public class TrashCounter : BaseCounter
+{
 
 
     public static event EventHandler OnAnyObjectTrashed;
 
-    new public static void ResetStaticData() {
+    new public static void ResetStaticData()
+    {
         OnAnyObjectTrashed = null;
     }
 
 
 
-    public override void Interact(Player player) {
-        if (player.HasKitchenObject()) {
-            player.GetKitchenObject().DestroySelf();
-
-            OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
+    public override void Interact(Player player)
+    {
+        if (player.HasKitchenObject())
+        {
+            KitchenObject.DestroyKitchenObject(player.GetKitchenObject());
+            InteractLogicServerRpc();
         }
+    }
+
+    [ServerRpc(RequireOwnership = false)]
+    private void InteractLogicServerRpc()
+    {
+        IntercatLogicClientRpc();
+    }
+
+    [ClientRpc]
+    private void IntercatLogicClientRpc()
+    {
+        OnAnyObjectTrashed?.Invoke(this, EventArgs.Empty);
     }
 
 }
--- Assets/Scripts/KitchenObject.cs
+++ Assets/Scripts/KitchenObject.cs
@@ -78,9 +78,12 @@ public class KitchenObject : NetworkBehaviour
 
     public void DestroySelf()
     {
-        kitchenObjectParent.ClearKitchenObject();
-
         Destroy(gameObject);
+    }
+
+    public void ClearKitchenObjectOnParent()
+    {
+        kitchenObjectParent.ClearKitchenObject();
     }
 
     public bool TryGetPlate(out PlateKitchenObject plateKitchenObject)
@@ -102,6 +105,11 @@ public class KitchenObject : NetworkBehaviour
     public static void SpawnKitchenObject(KitchenObjectSO kitchenObjectSO, IKitchenObjectParent kitchenObjectParent)
     {
         KitchenGameMultiplayer.Instance.SpawnKitchenObject(kitchenObjectSO, kitchenObjectParent);
+    }
+
+    public static void DestroyKitchenObject(KitchenObject kitchenObject)
+    {
+        KitchenGameMultiplayer.Instance.DestroyKitchenObject(kitchenObject);
     }
 
 }
